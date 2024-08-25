d4a7c39cdfffeb09c70c883e7a09b741ad9b2196
TheDJGamerYT <156543381+TheDJGamerYT@users.noreply.github.com>
2024-08-25 00:44:49 -0600

    initial mod commit iguess

---
+++ src/client/java/dj/aic/AdventuresInCrimeClient.java
@@ -0,0 +1,10 @@
+package dj.aic;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AdventuresInCrimeClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
