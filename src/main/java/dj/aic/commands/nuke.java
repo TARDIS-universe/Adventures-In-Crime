3f10644133f731d0baaca5e1e7404fba61e52744
TheDJGamerYT <156543381+TheDJGamerYT@users.noreply.github.com>
2024-08-26 07:36:38 -0500

    idk i made this before college today

--- src/main/java/dj/aic/commands/nuke.java
+++ src/main/java/dj/aic/commands/nuke.java
@@ -7,6 +7,7 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.suggestion.SuggestionProvider;
+
 public class nuke extends command {
    public static
 
